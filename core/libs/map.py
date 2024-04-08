704cd16a835a3a7978a4c09680ffbf850309d8dc
EarthlyEric <earthlyeric6@gmail.com>
2024-04-09 00:25:41 +0800

    Update Map class to disable zoom control and remove unnecessary code

--- core/libs/map.py
+++ core/libs/map.py
@@ -7,8 +7,8 @@ from selenium import webdriver
 class Map():
     @classmethod
     async def generate(cls, location: list):
-        map = folium.Map(location=location, zoom_start=5)
-        pin = folium.Marker(location=location).add_to(map)
+        map = folium.Map(location=location, zoom_start=5,zoom_control=False)
+        folium.Marker(location=location).add_to(map)
 
         filename = str(uuid.uuid4())
         temp_dir = os.path.abspath("./temp")
@@ -19,20 +19,16 @@ class Map():
         map.save(os.path.join(temp_dir, f"{filename}.html"))
 
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless")  
-        options.add_argument("--disable-gpu") 
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-infobars")
-        options.add_argument("--start-maximized")
-        options.add_argument("--disable-notifications")
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
 
         driver =  webdriver.Chrome(options=options)
-        driver.get("file://" + os.path.abspath(f"/temp/{filename}.html"))
-        await asyncio.sleep(2)
+        driver.get("file://" + os.path.abspath(f"./temp/{filename}.html"))
+        await asyncio.sleep(4)
         driver.set_window_size(1280, 720)
-        screenshot = driver.get_screenshot_as_base64()
+        screenshot = driver.get_screenshot_as_base64().encode("utf-8")
         driver.quit()
-
+        os.remove(os.path.join(temp_dir, f"{filename}.html"))
         return screenshot
