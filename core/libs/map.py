b70fa7815c0c6215218249e0470cc178789e4f46
EarthlyEric <earthlyeric6@gmail.com>
2024-04-08 22:08:42 +0800

    Add Map class for generating maps and screenshots

---
+++ core/libs/map.py
@@ -0,0 +1,27 @@
+import os
+import asyncio
+import uuid
+import folium
+from selenium import webdriver
+from concurrent.futures.thread import ThreadPoolExecutor
+
+
+class Map():
+    @classmethod
+    async def generate(cls, location: list):
+        map = folium.Map(location=location, zoom_start=5)
+        pin = folium.Marker(location=location).add_to(map)
+
+        filename = str(uuid.uuid4())
+        map.save(os.path.abspath(f"/temp/{filename}.html"))
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        driver =  webdriver.Chrome(options=options)
+        driver.get("file://" + os.path.abspath(f"/temp/{filename}.html"))
+        await asyncio.sleep(2)
+        driver.set_window_size(1280, 720)
+        screenshot = driver.get_screenshot_as_base64()
+        driver.quit()
+
+        return screenshot
--- router/map_gen.py
+++ router/map_gen.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI,APIRouter
 
 from core.config import Config
+from core.libs.map import Map
 
 
 mapGen = APIRouter()
 
 @mapGen.get("/generate/{pos}")
 async def generate_map(pos: str):
-    config=Config()
+    config = Config()
     pos1,pos2 = pos.split(",")
-    return {"version": config.version, "pos1": pos1, "pos2": pos2}
+    location = [pos1,pos2]
+    image = await Map.generate(location)
+
+    return {
+        "version": config.version, 
+        "pos1": pos1, "pos2": pos2, 
+        "image": image}
