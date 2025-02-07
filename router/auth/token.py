4ac58a94edf2ad9f4bf1c1c8c6f4b5efaf070dea
EarthlyEric <earthlyeric6@gmail.com>
2025-02-08 02:11:16 +0800

    remove unused variables

--- router/auth/token.py
+++ router/auth/token.py
@@ -3,16 +3,14 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+
 from core.database import getSession
-from core.database.model import ApiKey
+from core.database.schema import ApiKey
 from core.utils.tokenGen import createAccessToken
-import uuid
 
 token = APIRouter(
     prefix="/token",
 )
-
-fake_api_key = "1234"
 
 class generateJWTtokenResponse(BaseModel):
     apiKey: str
@@ -22,12 +20,11 @@ class generateJWTtokenResponse(BaseModel):
             description="Generate a JWT token for the user's API key",
             )
 async def generateJWTtoken(response: generateJWTtokenResponse, database: AsyncSession = Depends(getSession)):
-    cmd = select(ApiKey).where(ApiKey.apiKey == response.apiKey)
     async with database as session:
-        result = await session.execute(cmd)
+        result = await session.execute(select(ApiKey).where(ApiKey.apiKey == response.apiKey))
     item = result.scalars().first()
     if item is None:
         raise HTTPException(status_code=401, detail="Your API key is invalid")
     else:
-        return {"accessTocken": createAccessToken({"uuid": item.uuid, "apiKey": item.apiKey})}
+        return {"accessTocken": createAccessToken({"uuid": str(item.uuid), "apiKey": item.apiKey})}
     
